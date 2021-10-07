e3dbe0a2b168a1f589a2c9c9685b3a53d15cb404
joaovsc10 <57575464+joaovsc10@users.noreply.github.com>
2021-10-07 11:30:57 +0100

    Update warning.php

--- warnings/warning.php
+++ warnings/warning.php
@@ -70,5 +70,66 @@ class Warning{
     return false;
 		  
 }
+
+function searchByStartEndDate($data){
+  
+
+	if($data['start_date']!=$data['end_date'])
+	{
+    $query = "SELECT
+                *
+            FROM
+                " . $this->table_name . "
+            WHERE 
+			
+			patient_number = :patient_number AND
+			
+			warning_date 
+			
+			BETWEEN
+			
+                :start_date
+			AND
+				:end_date
+	";
+	
+	$startEndDifferent=true;
+	
+	
+	}
+	else{
+		
+		$query = "SELECT
+                *
+            FROM
+                " . $this->table_name . " w
+            WHERE 
+			
+			DATE(w.warning_date)= :start_date
+			
+			AND
+			
+			patient_number=:patient_number
+	";
+	$startEndDifferent=false;
+		
+	}
+	 $stmt = $this->conn->prepare( $query );
+	 
+	if($startEndDifferent)
+		$stmt->bindParam(":end_date", $data['end_date']);
+	
+	$stmt->bindParam(":start_date", $data['start_date']);
+	$stmt->bindParam(":patient_number", $data['patient_number']);
+  
+    // prepare query statement
+   
+	
+    // execute query
+    if($stmt->execute()){
+	
+		return $stmt;
+	}
+	
 }
-?>
+}
