5c6a7862dd52c26c0505a2623f6554b830fa5ef5
joaovsc10 <joaovitorcoelho10@gmail.com>
2021-10-16 16:26:28 +0100

    changing to $_POST

--- readings/create_reading.php
+++ readings/create_reading.php
@@ -5,95 +5,93 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  
+
 require_once 'sensors.php';
 require_once (__DIR__.'\..\db\db_connect.php');
 
 $emptyFound=FALSE;
-  
+
 $database = new DbConnect();
 $db = $database->getConnection();
-  
+
 $sensors = new Sensors($db);
-  
-  // get posted data
-$queries = array();
-parse_str($_SERVER['QUERY_STRING'], $queries);
-  
+
+
 
 // make sure data is not empty
-foreach($queries as $element) {
+foreach($_POST as $element) {
 
 	if(empty($element) && $element!=0){
 		$emptyFound=TRUE;
 	}
-	
+
 }
+
 if(!$emptyFound)
 {
     // set sensors property values
-    $sensors->S1 = $queries["S1"];
-    $sensors->S2 = $queries["S2"];
-	$sensors->S3 = $queries["S3"];
-	$sensors->S4 = $queries["S4"];
-	$sensors->S5 = $queries["S5"];
-	$sensors->S6 = $queries["S6"];
-	$sensors->S7 = $queries["S7"];
-	$sensors->S8 = $queries["S8"];
-	$sensors->S9 = $queries["S9"];
-	$sensors->S10 =$queries["S10"];
-	$sensors->S11 = $queries["S11"];
-	$sensors->S12 = $queries["S12"];
-	$sensors->S13 = $queries["S13"];
-	$sensors->S14 = $queries["S14"];
-	$sensors->S15 = $queries["S15"];
-	$sensors->S16 = $queries["S16"];
-	$sensors->S17 = $queries["S17"];
-	$sensors->S18 = $queries["S18"];
-	$sensors->S19 = $queries["S19"];
-	$sensors->S20 = $queries["S20"];
-	$sensors->S21 = $queries["S21"];
-	$sensors->S22 = $queries["S22"];
-	$sensors->S23 = $queries["S23"];
-	$sensors->S24 = $queries["S24"];
-	$sensors->S25 = $queries["S25"];
-	$sensors->S26 = $queries["S26"];
-	$sensors->T1 = $queries["T1"];
-	$sensors->T2 = $queries["T2"];
-	$sensors->H1 = $queries["H1"];
-	$sensors->H2 = $queries["H2"];
-	$sensors->date = $queries["date"];
-	$sensors->patient_number = $queries["patient_number"];
-  
+    $sensors->S1 = $_POST["S1"];
+    $sensors->S2 = $_POST["S2"];
+	$sensors->S3 = $_POST["S3"];
+	$sensors->S4 = $_POST["S4"];
+	$sensors->S5 = $_POST["S5"];
+	$sensors->S6 = $_POST["S6"];
+	$sensors->S7 = $_POST["S7"];
+	$sensors->S8 = $_POST["S8"];
+	$sensors->S9 = $_POST["S9"];
+	$sensors->S10 =$_POST["S10"];
+	$sensors->S11 = $_POST["S11"];
+	$sensors->S12 = $_POST["S12"];
+	$sensors->S13 = $_POST["S13"];
+	$sensors->S14 = $_POST["S14"];
+	$sensors->S15 = $_POST["S15"];
+	$sensors->S16 = $_POST["S16"];
+	$sensors->S17 = $_POST["S17"];
+	$sensors->S18 = $_POST["S18"];
+	$sensors->S19 = $_POST["S19"];
+	$sensors->S20 = $_POST["S20"];
+	$sensors->S21 = $_POST["S21"];
+	$sensors->S22 = $_POST["S22"];
+	$sensors->S23 = $_POST["S23"];
+	$sensors->S24 = $_POST["S24"];
+	$sensors->S25 = $_POST["S25"];
+	$sensors->S26 = $_POST["S26"];
+	$sensors->T1 = $_POST["T1"];
+	$sensors->T2 = $_POST["T2"];
+	$sensors->H1 = $_POST["H1"];
+	$sensors->H2 = $_POST["H2"];
+	$sensors->date = $_POST["date"];
+	$sensors->patient_number = $_POST["patient_number"];
+
     // create the sensors
     if($sensors->create()){
-  
+
 
         // set response code - 201 created
         http_response_code(201);
-  
+
         // tell the user
         echo json_encode(array("success" => "1","message" => "Sensors reading was created.", "reading_id" => $db->lastInsertId() , "date" => $sensors->date));
 
     }
-  
+
     // if unable to create the sensors, tell the user
     else{
-  
+
         // set response code - 503 service unavailable
         http_response_code(503);
-  
+
         // tell the user
         echo json_encode(array("success" => "0","message" => "Unable to create sensors reading."));
     }
 }
-  
+
 // tell the user data is incomplete
 else{
-  
+
     // set response code - 400 bad request
     http_response_code(400);
-  
+
     // tell the user
     echo json_encode(array("success" => "0", "message" => "Unable to create sensors reading. Data is incomplete."));
 }
--- statistics/create_statistics.php
+++ statistics/create_statistics.php
@@ -5,72 +5,67 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  
+
 require_once 'statistics.php';
 require_once (__DIR__.'\..\db\db_connect.php');
 
 $emptyFound=FALSE;
-  
+
 $database = new DbConnect();
 $db = $database->getConnection();
-  
+
 $statistics = new Statistics($db);
-  
-  // get posted data
-$queries = array();
-parse_str($_SERVER['QUERY_STRING'], $queries);
-  
+
 
 // make sure data is not empty
-foreach($queries as $element) {
+foreach($_POST as $element) {
 
 	if(empty($element) && $element!=0){
 		$emptyFound=TRUE;
 	}
-	
+
 }
 if(!$emptyFound)
 {
     // set statistics property values
-  
-    $statistics->left_foot_stance = $queries["left_foot_stance"];
 
-    $statistics->right_foot_stance = $queries["right_foot_stance"];
-    $statistics->cadence = $queries["cadence"];
-    $statistics->balance = $queries["balance"];
-	$statistics->steps = $queries["steps"];
-	$statistics->date = $queries["date"];
-	$statistics->patient_number = $queries["patient_number"];
-  
+  $statistics->left_foot_stance = $_POST["left_foot_stance"];
+  $statistics->right_foot_stance = $_POST["right_foot_stance"];
+  $statistics->cadence = $_POST["cadence"];
+  $statistics->balance = $_POST["balance"];
+	$statistics->steps = $_POST["steps"];
+	$statistics->date = $_POST["date"];
+	$statistics->patient_number = $_POST["patient_number"];
+
     // create the statistics
     if($statistics->create()){
-  
+
 
         // set response code - 201 created
         http_response_code(201);
-  
+
         // tell the user
         echo json_encode(array("success" => "1","message" => "Statistics record was created.", "statistics_id" => $db->lastInsertId() , "date" => $statistics->date));
 
     }
-  
+
     // if unable to create the statistics, tell the user
     else{
-  
+
         // set response code - 503 service unavailable
         http_response_code(503);
-  
+
         // tell the user
         echo json_encode(array("success" => "0","message" => "Unable to create statistics reading."));
     }
 }
-  
+
 // tell the user data is incomplete
 else{
-  
+
     // set response code - 400 bad request
     http_response_code(400);
-  
+
     // tell the user
     echo json_encode(array("success" => "0", "message" => "Unable to create statistics record. Data is incomplete."));
 }
